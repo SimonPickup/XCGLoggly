5c0f477eb4c541432c7f0087df4e8a0a0f36006b
Simon Pickup <simon@pickupinfinity.com>
2016-12-02 12:38:20 +0000

    Stub XCGLogglyDestination.swift

---
+++ XCGLoggly/XCGLogglyDestination.swift
@@ -0,0 +1,14 @@
+//
+//  XCGLogglyDestination.swift
+//  XCGLoggly
+//
+//  Created by Simon Pickup on 02/12/2016.
+//  Copyright © 2016 Pickup Infinity Limited. All rights reserved.
+//
+
+import Foundation
+import XCGLogger
+
+public class XCGLogglyDestination : XCGBaseLogDestination {
+    
+}
