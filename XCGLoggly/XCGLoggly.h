0eea46afe64f52952c7149280a1bbc6040c1f7fd
Simon Pickup <simon@pickupinfinity.com>
2016-12-02 10:27:00 +0000

    New Xcode Framework, set to Swift 2.3

---
+++ XCGLoggly/XCGLoggly.h
@@ -0,0 +1,19 @@
+//
+//  XCGLoggly.h
+//  XCGLoggly
+//
+//  Created by Simon Pickup on 02/12/2016.
+//  Copyright © 2016 Pickup Infinity Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XCGLoggly.
+FOUNDATION_EXPORT double XCGLogglyVersionNumber;
+
+//! Project version string for XCGLoggly.
+FOUNDATION_EXPORT const unsigned char XCGLogglyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XCGLoggly/PublicHeader.h>
+
+
